3938d46f4da705ec5d5f58004574fe193bfd0ae8
ArturM2005 <arturmuhamediarov@gmail.com>
2024-05-03 10:34:21 +0300

    Add form

--- Alay/Registration/views.py
+++ Alay/Registration/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django import forms
 # Create your views here.
 
+class Form(forms.Form):
+    Form = forms.CharField(label="Login")
 def index(request):
-    return render(request, 'Registration/index.html')
+    return render(request, 'Registration/index.html',{
+        'form':Form
+    })
