eef3e87124a93800196e24d23c8112b4a28865a1
ArturM2005 <arturmuhamediarov@gmail.com>
2024-05-03 11:39:39 +0300

    Added csrf and changed forms

--- Alay/Registration/views.py
+++ Alay/Registration/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django import forms
 # Create your views here.
 
-class Form(forms.Form):
-    Form = forms.CharField(label="Login")
+class Forms(forms.Form):
+    Reg = forms.CharField(label="Login")
 def index(request):
+    if request.method == 'POST':
+        form = Forms(request.POST)
+        if form.is_valid():
+            Reg = form.cleaned_data["Reg"]
     return render(request, 'Registration/index.html',{
-        'form':Form
+        'form':Forms
     })
